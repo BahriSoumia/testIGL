2290b78b7fb9c044658017fe69e6169f4106c38c
BahriSoumia <fs_bahri@esi.dz>
2017-10-14 19:19:12 +0100

    tp classes

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,16 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         Vector<Integer> vect = new Vector<Integer>();
+        System.out.println(vect.size());
         vect.add(0, 1);
-        vect.add(1,3000 );
+        vect.add(1,200 );
         vect.add(2, 700);
-        vect.add(3, 711);
-        vect.add(4, 12);
-        vect.add(5, 903);
-        vect.add(6, 22);
-        vect.add(7, 251);
-        vect.add(8, 53);
-        int u=0;
-
-        Vector<Integer> vect2 = VectorHelper.Inverser(vect);
-
-        for(int i=0; i<vect2.size(); i++){
-            System.out.println(vect2.get(i));
+        System.out.println(vect.size());
+         VectorHelper.Inverser(vect);
+        System.out.println(vect.size());
+        for(int i=0; i<vect.size(); i++){
+            System.out.println(vect.get(i));
         }
-
 
 
         System.out.println("\n");
--- src/VectorHelper.java
+++ src/VectorHelper.java
@@ -20,19 +20,17 @@ public class VectorHelper {
      * @return retourne un vecteur dant les elements sont inverses
      */
 
-    public static Vector<Integer> Inverser(Vector<Integer> vect){
-        int i,j,x;
-        int n=vect.size();
-        i=0;
-        j=n-1;
-        while(i<j){
-            x=vect.get(j);
-            vect.add(j, vect.get(i));
-            vect.add(i, x);
+    public static void Inverser(Vector<Integer> vect){
+        int i=0,x=0;
+        int j=vect.size()- 1;
+        while (i<j ){
+            x = vect.get(j);
+            vect.setElementAt( vect.get(i),j);
+            vect.setElementAt(x, i);
             i++;
             j--;
         }
-        return vect;
+
 
     }
 
@@ -45,24 +43,29 @@ public class VectorHelper {
      * @param vect vecteur des entiers
      */
 
-    public static void triBulleCroissant(Vector<Integer> vect) {
+    public static Vector<Integer> triBulleCroissant(Vector<Integer> vect) {
         int longueur = vect.size();
         int tampon = 0;
         boolean permut ;
-
+        int i;
         do{
+
             permut = false ;
-            for (int i = 0; i < longueur; i++) {
+            for ( i = 0; i < longueur - 2; i++) {
+                int elemi=vect.get(i);
+                int elemii=vect.get(i+1);
                 // Teste si 2 éléments successifs sont dans le bon ordre ou non
-                if (vect.get(i) > vect.get(i+1)) {
+                if (elemi > elemii) {
                     // s'ils ne le sont pas, on échange leurs positions
-                    tampon = vect.get(i);
-                    vect.add(i, vect.get(i+1));
-                    vect.add(i+1, tampon);
+                    tampon = elemi;
+                    vect.setElementAt( elemii,i);
+                    vect.setElementAt(tampon,i+1);
                     permut = true;
                 }
 
             }
-        } 	while (permut) ;
+        } 	while (permut && (i<longueur-1)) ;
+        return vect;
     }
+
 }
